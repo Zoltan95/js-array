41e0d083161a6236bbcdc129fb0ca91ee0cda3e7
Zoltan Urban <56049513+Zoltan95@users.noreply.github.com>
2023-07-14 17:13:04 +0100

    Fetching images from API

--- js/js-array.js
+++ js/js-array.js
@@ -1,12 +1,14 @@
 const listOfPicturesBody = document.getElementById("list-of-pictures");
+let urlMain = "https://picsum.photos/v2/list?page=2&limit=100";
 
-window.onload = () => {
-    url_string = "https://picsum.photos/v2/list?page=1&limit=100";
-    url = new URL(url_string);
-    options = url.searchParams.getAll("options[]");
-    console.log(options);
-};
-
-fetch ('https://picsum.photos/v2/list?page=1&limit=100')
+fetch (urlMain)
     .then (res => res.json())
-    .then (data => listOfPicturesBody = data)
+    .then (data => generateImages(data))
+
+function generateImages(data) {
+    const options = data.map(item => 
+        //console.log(item)
+        `<img class="test" src='${item.download_url}' alt='${item.author}' width="500" height="500">`
+    ).join('');
+    listOfPicturesBody.innerHTML = options;
+}
