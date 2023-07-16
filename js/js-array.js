4471abce67920745f0f2f70c2b4830709e8b4207
Zoltan Urban <56049513+Zoltan95@users.noreply.github.com>
2023-07-16 17:15:53 +0100

    Displaying Random Images with button Click

--- js/js-array.js
+++ js/js-array.js
@@ -1,14 +1,37 @@
+
+const loadImageButton = document.getElementById("loadImage");
 const listOfPicturesBody = document.getElementById("list-of-pictures");
-let urlMain = "https://picsum.photos/v2/list?page=2&limit=100";
 
-fetch (urlMain)
-    .then (res => res.json())
-    .then (data => generateImages(data))
-
-function generateImages(data) {
-    const options = data.map(item => 
-        //console.log(item)
-        `<img class="test" src='${item.download_url}' alt='${item.author}' width="500" height="500">`
-    ).join('');
-    listOfPicturesBody.innerHTML = options;
+function fetchData(url){
+    return fetch(url)
+        //.then (data => console.log(data))
+        //.then (data => generateImageHTML(data.url, data.type))
 }
+
+fetchData("https://picsum.photos/1000/1000")
+    .then (data => generateImageHTML(data.url, data.type))
+
+function generateImageHTML(dataUrl, dataType) {
+    const html = 
+    `<img class="test" id="img" src='${dataUrl}' alt='${dataType}' width="500" height="500">`
+    ;
+    listOfPicturesBody.innerHTML = html;
+}
+
+function generateImage() {
+    const img = document.getElementById("img");
+    fetchData("https://picsum.photos/1000/1000")
+        .then (data => {
+            img.src = data.url;
+            img.alt = data.type;
+        })
+}
+
+loadImageButton.addEventListener("click", generateImage);
+//function generateImage(data) {
+    //const options = data.map(item => 
+        //console.log(item)
+       // `<img class="test" src='${item.url}' alt='${item.type}' width="500" height="500">`
+    //).join('');
+    //listOfPicturesBody.innerHTML = options;
+//}
